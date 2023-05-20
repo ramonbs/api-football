faf6338973c8cecca4062d33263f167f60fcf234
ramonbs <ramongiovanii12@gmail.com>
2023-05-20 20:33:45 -0300

    feat: add logic into input

--- src/components/LoginForms/index.tsx
+++ src/components/LoginForms/index.tsx
@@ -1,9 +1,21 @@
-import style from './loginForms.module.scss';
+import style from "./loginForms.module.scss";
+import { useState, FormEvent } from "react";
 
 function LoginForms() {
+    const [apiKey, setApiKey] = useState("");
+
+    const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        const form = e.target as HTMLFormElement;
+        const input = form.elements.namedItem("api_key") as HTMLInputElement;
+        setApiKey(input.value);
+        console.log(input.value);
+        
+    };
+
     return (
-        <form className={ style.login_form }>
-            <label htmlFor="api_key" className={ style.login_label}>
+        <form className={style.login_form} onSubmit={handleSubmit}>
+            <label htmlFor="api_key" className={style.login_label}>
                 API Key
                 <input
                     name="api_key"
@@ -11,6 +23,9 @@ function LoginForms() {
                     placeholder="API Key"
                 />
             </label>
+            <button type="submit" className={style.login_button}>
+                Entrar
+            </button>
         </form>
     );
 }
